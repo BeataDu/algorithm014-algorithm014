4ddfaf3b22ad77e85a82ab133635c04f84da5605
duxu <duxu_3113370047@163.com>
2020-10-06 23:07:39 +0800

    第八周作业提交

---
+++ Week_08/ReverseBitsObj.java
@@ -0,0 +1,12 @@
+package test20201006;
+
+public class ReverseBitsObj {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res = (res << 1) + (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
